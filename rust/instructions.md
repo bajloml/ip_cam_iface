f3c97e3c6d51de440a5338bbb160fa24c22798e6
Marko Bajlo <markobajlo123@gmail.com>
2022-08-03 20:18:59 +0200

    added Rust camera setup

--- rust/instructions.md
+++ rust/instructions.md
@@ -1,8 +1,15 @@
 ### instruction to build Rust application:
+-   install dependency thirtyfour, run ```cargo install thirtyfour```
 -   in the project folder (rust) execute ```cargo new interface --bin``` to create new binary package
 -   in the configuration file(Cargo.toml) add info about package like name, version, author...
+-   add dependencies at the end of the Cargo.toml:
+    ```[dependencies]```
+    ```tokio = {version = "1", features = ["full"]}```
+    ```thirtyfour = "0.30.0"```
 -   run ```cargo build```to build application/package
 -   run ```cargo run``` to run the application 
 
 ## alternative to cargo packaging is using an compiler:
 -   ```rustc src/interface.rs``` --> the output will be in the folder from where the compiler has been called
+
+## before run make sure chromedriver is running
--- rust/interface/src/main.rs
+++ rust/interface/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+
+    /* login auth*/
+    let username = String::from("admin");
+    let password = String::from("admin");
+
+    /* image name */
+    let img_name = String::from("cam_img");
+
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+
+    /* go to camera IP */
+    driver.goto("http://192.168.8.155").await?;
+
+    /* Login as user */
+    let element = driver.find(By::XPath("//input[@value='Applet' ]")).await?;
+    element.click().await?;
+
+    let element = driver.find(By::Name("ID")).await?;
+    element.clear().await?;
+    element.send_keys(username).await?;
+
+    let element = driver.find(By::Name("PassWord")).await?;
+    element.clear().await?;
+    element.send_keys(password).await?;
+
+    let element = driver.find(By::XPath("/html/body/form/table/tbody/tr/td/table//tbody/tr/td/table/tbody/tr/td/p/font/input")).await?;
+    element.click().await?;
+
+    /* go to camera settings */
+    driver.goto("http://192.168.8.155/sysconfig.cgi").await?;
+
+    /* enable image on local network */
+    let element = driver.find(By::XPath("/html/body/form/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr/td/input[@name='access_image' and @value=1]")).await?;
+    element.click().await?;
+
+    let element = driver.find(By::Name("img_name_drt_acs")).await?;
+    element.clear().await?;
+    element.send_keys(img_name).await?;
+
+    let element = driver.find(By::XPath("/html/body/form/table/tbody/tr/td/table/tbody/tr/td/div/input[@value='Submit']")).await?;
+    element.click().await?;
+   
+    // Always explicitly close the browser.
+    driver.quit().await?;
+
+    Ok(())
 }

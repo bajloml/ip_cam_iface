a9150a1c4c34ef2e1757c7f9aa259c86f6469537
Marko Bajlo <markobajlo123@gmail.com>
2022-08-14 20:06:12 +0200

    endian testing-overflow testing

--- python_test_endian/byte_test.py
+++ python_test_endian/byte_test.py
@@ -1,6 +1,7 @@
 import os
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
 
 def detect_overflow(value, old_value, overflow, overflow_step):
     ret = False
@@ -37,14 +38,14 @@ def detect_overflow2(value, old_value, overflow, overflow_step):
 # create exp function without 2x overflows in one step
 def create_exp(num_of_samples, words_in_sample, x_axis):
     num = 0
-    power = 2
+    power = 3
     data = []
 
     for i in range(int(num_of_samples * words_in_sample)):
         if ((i!=0) and i%words_in_sample==0):
             num+=1
 
-        data.append((x_axis[num] + (i%words_in_sample * 10))**power)
+        data.append((x_axis[num] + (i%words_in_sample * 15))**power)
     
     return data
 
@@ -70,11 +71,64 @@ def create_linear(num_of_samples, words_in_sample):
     data = []
 
     for i in range(int(num_of_samples * words_in_sample)):
-        data.append(num*5)
+        data.append(num*500)
         num+=1
 
     return data
 
+# read binary file as chosen endian:
+def bytes_to_list(bytes, endian, words_in_sample):
+
+    WORDS_list = [[] for _ in range(words_in_sample)]
+    word = 0
+    read_byte_from = 0
+    read_byte_to = 2
+    for i in range(word_samples * words_in_sample):
+        WORDS_list[word].append(int.from_bytes(bytes[read_byte_from:read_byte_to], endian))
+        read_byte_from = read_byte_to
+        read_byte_to +=2 
+        
+        word += 1
+
+        if(word>5):
+            word = 0
+
+    return WORDS_list
+
+# reconstruct data from list
+def reconstruct_data(from_list, words_in_sample, uint16_size):
+    WORDS_reconstructed = [[] for _ in range(words_in_sample)]
+    word = 0
+    for words in from_list:
+        overflow = 0
+        first_run_finished = False
+        old_value = 0
+
+        sample_counter = 0
+
+        for value in words:
+            if first_run_finished == False:
+                WORDS_reconstructed[word].append(value)
+                old_value = value
+
+            if(detect_overflow(value, old_value, overflow, uint16_size) and first_run_finished):
+                overflow +=1
+
+            if first_run_finished:
+                WORDS_reconstructed[word].append(value + overflow*uint16_size)
+                old_value = value + overflow*uint16_size
+
+            sample_counter += 1
+            first_run_finished = True  
+
+        word += 1
+
+        if(word>5):
+            word = 0
+
+    return WORDS_reconstructed
+
+print("system byteorder(endianness) is " + sys.byteorder)
 
 file_name = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + "/test.log"
 
@@ -97,7 +151,6 @@ data = create_exp(num_of_samples, words_in_sample, x_axis)
 
 data_arr_16 = np.array(data).astype(np.uint16)
 dummy_bytes = bytearray(data_arr_16)
-print("dummy bytes = " + str(dummy_bytes))
 
 # write dummy bytes to file
 try:
@@ -111,59 +164,19 @@ except IOError:
 # open file and read bytes into the arrays
 try:
     with open(file_name, "rb") as file:
-        WORDS_in_big = [[] for _ in range(words_in_sample)]
-        WORDS_in_little = [[] for _ in range(words_in_sample)]
-
         bytes = file.read()
         bytes_len = len(bytes)
         word_samples = int(bytes_len/bytes_to_read)
         print("bytes len = " + str(len(bytes)) + ", samples: " + str(word_samples))
 
-        # endian big
-        endian = "big"
-        word = 0
-        read_byte_from = 0
-        read_byte_to = 2
-        for i in range(word_samples * words_in_sample):
-            WORDS_in_big[word].append(int.from_bytes(bytes[read_byte_from:read_byte_to], endian))
-            read_byte_from = read_byte_to
-            read_byte_to +=2 
-            
-            word += 1
+        # read bytes in big endian byteorder
+        WORDS_in_big = bytes_to_list(bytes, "big", words_in_sample)
 
-            if(word>5):
-                word = 0
-
-        print("WORDS_in_big:")
-        i = 0
-        for words in WORDS_in_big:
-            print("WORD" + str(i) +": " + str(words))
-            i+=1
-
-        # endian little
-        endian = "little"
-        word = 0
-        read_byte_from = 0
-        read_byte_to = 2
-        for i in range(word_samples * words_in_sample):
-            WORDS_in_little[word].append(int.from_bytes(bytes[read_byte_from:read_byte_to], endian))
-            read_byte_from = read_byte_to
-            read_byte_to +=2 
-            
-            word += 1
-
-            if(word>5):
-                word = 0
-
-        print("WORDS_in_little:")
-        i = 0
-        for words in WORDS_in_little:
-            print("WORD" + str(i) +": " + str(words))
-            i+=1
+        # read bytes in little endian byteorder
+        WORDS_in_little = bytes_to_list(bytes, "little", words_in_sample)
 
 except IOError:
     print("Error while opening for reading, file -> " + file_name)
-
 
 # ploting in overflow, (uint16)
 fig, axis = plt.subplots(2)
@@ -179,90 +192,40 @@ for words in WORDS_in_little:
     axis[1].plot(x_axis, words, label="little_val" + str(i))
     i+=1
 
-axis[0].set_title("WORDS_in_big_endian")
-axis[1].set_title("WORDS_in_little_endian")
+axis[0].set_title("WORDS_in_big_endian, system is: " + sys.byteorder)
+axis[1].set_title("WORDS_in_little_endian, system is: " + sys.byteorder)
 axis[0].legend()
 axis[1].legend()
 plt.show(block=False)
 
 # reconstruct values from uint16
-WORDS_in_big_test = [[] for _ in range(words_in_sample)]
-WORDS_in_little_test = [[] for _ in range(words_in_sample)]
+WORDS_big_endian_reconstructed = reconstruct_data(WORDS_in_big, words_in_sample, uint16_size)
+WORDS_little_endian_reconstructed = reconstruct_data(WORDS_in_little, words_in_sample, uint16_size)
 
-word = 0
-for words in WORDS_in_big:
-    overflow = 0
-    first_run_finished = False
-    old_value = 0
+print("WORDS_big_reconstructed:")
+for i in range(len(WORDS_big_endian_reconstructed)):
+    print("WORD" + str(i) +": " + str(WORDS_big_endian_reconstructed[i]))
 
-    sample_counter = 0
-
-    for value in words:
-        if first_run_finished == False:
-            WORDS_in_big_test[word].append(value)
-            old_value = value
-
-        if(detect_overflow(value, old_value, overflow, uint16_size) and first_run_finished):
-            overflow +=1
-        # overflow = detect_overflow2(value, old_value, overflow, uint16_size)
-
-        if first_run_finished:
-            WORDS_in_big_test[word].append(value + overflow*uint16_size)
-            old_value = value + overflow*uint16_size
-
-        sample_counter += 1
-        first_run_finished = True  
-
-    word += 1
-
-    if(word>5):
-        word = 0
-
-word = 0
-for words in WORDS_in_little:
-    overflow = 0
-    first_run_finished = False
-    old_value = 0
-
-    sample_counter = 0
-
-    for value in words:
-        if first_run_finished == False:
-            WORDS_in_little_test[word].append(value)
-            old_value = value
-
-        if(detect_overflow(value, old_value, overflow, uint16_size) and first_run_finished):
-            overflow +=1
-        # overflow = detect_overflow2(value, old_value, overflow, uint16_size)
-
-        if first_run_finished:
-            WORDS_in_little_test[word].append(value + overflow*uint16_size)
-            old_value = value + overflow*uint16_size
-
-        sample_counter += 1
-        first_run_finished = True  
-
-    word += 1
-
-    if(word>5):
-        word = 0
+print("WORDS_little_reconstructed:")
+for i in range(len(WORDS_little_endian_reconstructed)):
+    print("WORD" + str(i) +": " + str(WORDS_little_endian_reconstructed[i]))
 
 # ploting in reconstructed values
 fig, axis = plt.subplots(2)
 fig.suptitle("big and little endian")
 
 i = 0
-for words in WORDS_in_big_test:
+for words in WORDS_big_endian_reconstructed:
     axis[0].plot(x_axis, words, label="big_val" + str(i))
     i+=1
 
 i = 0
-for words in WORDS_in_little_test:
+for words in WORDS_little_endian_reconstructed:
     axis[1].plot(x_axis, words, label="little_val" + str(i))
     i+=1
 
-axis[0].set_title("WORDS_in_big_endian")
-axis[1].set_title("WORDS_in_little_endian")
+axis[0].set_title("WORDS_big_endian_reconstructed, system is: " + sys.byteorder)
+axis[1].set_title("WORDS_little_endian_reconstructed, system is: " + sys.byteorder)
 axis[0].legend()
 axis[1].legend()
 plt.show()
